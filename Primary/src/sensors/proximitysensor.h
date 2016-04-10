f15613c43e930eeb30d1385daf7f638a390bed42
geoffreyknopf777 <geoffreyknopf@gmail.com>
2016-04-10 16:19:51 -0700

    update

--- Primary/src/sensors/proximitysensor.h
+++ Primary/src/sensors/proximitysensor.h
@@ -9,6 +9,7 @@
 #define PROXIMITYSENSOR_H
 
 #include <stdbool.h>
+#include <inttypes.h>
 
 /* Initialize the proximity sensor module */
 void ProximitySensorInit(volatile uint8_t* pDDR_Set, volatile uint8_t* pPORT_Set, volatile uint8_t* pPIN_Set, unsigned char nPinNum_Set);
