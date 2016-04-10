0627942d87057985a3458041d1e6aadfabcbbca2
geoffreyknopf777 <geoffreyknopf@gmail.com>
2016-04-10 16:51:36 -0700

    test led

--- Primary/src/main/main.c
+++ Primary/src/main/main.c
@@ -15,6 +15,7 @@
 #include "temperaturesensor.h"
 #include "proximitysensor.h"
 #include "smartled.h"
+#include "led.h"
 #include "relay.h"
 #include "timer.h"
 #include "test.h"
@@ -22,6 +23,7 @@
 #define UART_BAUD_RATE 9600
 #define MAX_MSG_LEN 256
 
+LED uTestLed; //test led
 char sSendMsg[MAX_MSG_LEN]; //send buffer
 char sRecMsg[MAX_MSG_LEN]; //receive buffer
 
@@ -32,6 +34,7 @@ void RunTests(void){
 struct SmartLED uLed;
 
 void Init(void){
+	LedInit(&uTestLed, &DDRB, &PORTB, 1); //test led
 	UARTInit(UART_BAUD_RATE); //uart init
 	UARTMuxInit(&DDRD, &PORTD, 4, &DDRD, &PORTD, 5, &DDRD, &PORTD, 6); //uart mux init
 	TemperatureSensorInit(&DDRC, &PORTC, 0); //temperature sensor init
@@ -102,10 +105,11 @@ int main(void)
 		
 		if(ProximitySensorInRange()){
 			//code gets here if the proximity sensor is in range
+			LedBlink(&uLedTest, 100);
 			RelayTurnOn0();
 		}
 		else{
-			RelayTurnOff0();
+			LedBlink(&uLedTest, 1000);
 		}
 	}
   
