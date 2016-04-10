8daa02333759df6f347f5cfaaa368f50a56541e2
geoffreyknopf777 <geoffreyknopf@gmail.com>
2016-04-10 16:23:48 -0700

    update

--- Primary/src/sensors/proximitysensor.c
+++ Primary/src/sensors/proximitysensor.c
@@ -14,7 +14,7 @@ unsigned char nPinNum;
 /* Initialize the proximity sensor module */
 void ProximitySensorInit(volatile uint8_t* pDDR_Set, volatile uint8_t* pPORT_Set, volatile uint8_t* pPIN_Set, unsigned char nPinNum_Set){
 	//get settings
-	pPIN = pPIN_SET;
+	pPIN = pPIN_Set;
 	
 	//set pin as input
 	ClearBits(*pDDR_Set, nPinNum_Set, 1);
