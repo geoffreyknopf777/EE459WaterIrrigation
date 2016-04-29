66acdc29df09c4642d8f78c9503e6766788238ec
geoffreyknopf777 <geoffreyknopf@gmail.com>
2016-04-28 21:57:45 -0700

    Prox test

--- Primary/src/main/main.c
+++ Primary/src/main/main.c
@@ -130,27 +130,35 @@ int main(void)
 		//Control the valves
 		if(sZone1[0] == '1' /* && nTemp > TEMP_MIN */ && cMoisture > MOISTURE_MAX  && cLight > LIGHT_MIN && cLight < LIGHT_MAX && !bProx){
 			RelayTurnOn0();
+			/*
 			for(i=0; i<SMART_LED_NUM/2; i++){
 				SmartLEDSetColor(COLOR_RED, i);
 			}
+			*/
 		}
 		else{
 			RelayTurnOff0();
+			/*
 			for(i=0; i<SMART_LED_NUM/2; i++){
 				SmartLEDSetColor(COLOR_NONE, i);
 			}
+			*/
 		}
 		if(sZone2[0] == '1' /* && nTemp > TEMP_MIN */ && cMoisture > MOISTURE_MAX && cLight > LIGHT_MIN && cLight < LIGHT_MAX && !bProx){
 			RelayTurnOn1();
+			/*
 			for(i=3; i<SMART_LED_NUM; i++){
 				SmartLEDSetColor(COLOR_RED, i);
 			}
+			*/
 		}
 		else{
 			RelayTurnOff1();
+			/*
 			for(i=3; i<SMART_LED_NUM; i++){
 				SmartLEDSetColor(COLOR_NONE, i);
 			}
+			*/
 		}	
 	//SmartLEDProcess();
 		
